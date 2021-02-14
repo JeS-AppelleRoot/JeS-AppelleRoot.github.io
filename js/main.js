ee0b2c1680f4b1f8fbf65f4f6bd52d1b6dbb8f26
AmanAtao <57495333+Amanatao@users.noreply.github.com>
2021-02-14 14:04:24 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function( $ ) {
   $('.countdown').each(function() {
     $(this).countdown({
     //    until: new Date($(this).attr('data-date'))
-      until: (86400)+7320-240-3600
+      until: 172680
   });
   });
 
