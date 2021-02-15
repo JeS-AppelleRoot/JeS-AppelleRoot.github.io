639970a097af255cd8643c2ce54e6812bc4975fe
AmanAtao <antoine.lenours@gmail.com>
2021-02-15 12:46:17 +0100

    Fin du 1er site

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ jQuery(document).ready(function( $ ) {
 
   $('.countdown').each(function() {
     $(this).countdown({
-        until: new Date($(this).attr('data-date'))
-    });
+    //    until: new Date($(this).attr('data-date'))
+      until: (86400*2)+7320
+  });
   });
 
 });
